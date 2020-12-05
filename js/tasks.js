7d6a372bf7829b04476c2f3fa255922102790208
Anna Shevchenko <uaskoa@gmail.com>
2020-12-05 14:10:53 +0200

    task-06

---
+++ js/tasks.js
@@ -0,0 +1,27 @@
+function checkForSpam(message) {
+  let result;
+  // Пиши код ниже этой строки
+  //console.log(message.toLowerCase());
+  //result = message.toLowerCase().includes("spam" || "sale");
+  console.log(message.toLowerCase().(includes"spam" || "sale"));
+
+  // Пиши код выше этой строки
+  return result;
+}
+
+i = a || b
+
+i = a }|| i = b
+
+checkForSpam("Latest technology news");
+checkForSpam("JavaScript weekly newsletter");
+checkForSpam("Get best sale offers now!");
+checkForSpam("Amazing SalE, only tonight!");
+checkForSpam("Trust me, this is not a spam message");
+checkForSpam("Get rid of sPaM emails. Our book in on sale!");
+checkForSpam("[SPAM] How to earn fast money?");
+
+// console.log(checkForSpam("Latest technology news"));
+// console.log(checkForSpam("JavaScript weekly newsletter"));
+// console.log(checkForSpam("Get best sale offers now!"));
+// console.log(checkForSpam("Amazing SalE, only tonight!"));
