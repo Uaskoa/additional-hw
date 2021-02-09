60c50a45a1819ee920e40614fbee8cb24b569908
Anna Shevchenko <uaskoa@gmail.com>
2021-02-09 22:57:51 +0200

    6-8

---
+++ js/module-6.js
@@ -0,0 +1,105 @@
+//////////////////////////////// №1 ////////////////////////
+
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+//   // Пиши код ниже этой строки
+
+//   orderedItems.forEach((item) => (totalPrice += item));
+
+//   // Пиши код выше этой строки
+//   return totalPrice;
+// }
+
+//////////////////////////////// №2 ////////////////////////
+
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+//   // Пиши код ниже этой строки
+
+//   numbers.forEach(function (number) {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+
+//   // Пиши код выше этой строки
+//   return filteredNumbers;
+// }
+
+//////////////////////////////// №3 ////////////////////////
+
+// function getCommonElements(firstArray, secondArray) {
+//     const commonElements = [];
+//     // Пиши код ниже этой строки
+
+//     firstArray.forEach(function (number) {
+//         if (secondArray.includes(number)) {
+//             commonElements.push(number)
+//         }
+//     })
+
+//     return commonElements;
+//     // Пиши код выше этой строки
+//   }
+
+//////////////////////////////// №4 ////////////////////////
+
+// Пиши код ниже этой строки
+
+// function calculateTotalPrice(quantity, pricePerItem) {
+//   // Пиши код выше этой строки
+//   return quantity * pricePerItem;
+// }
+
+// const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
+
+//////////////////////////////// №5 ////////////////////////
+
+// const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
+
+//////////////////////////////// №6 ////////////////////////
+
+
+
+// const calculateTotalPrice = (orderedItems) => {
+//   let totalPrice = 0;
+
+//   orderedItems.forEach((item) => {
+//     totalPrice += item;
+//   });
+
+//   return totalPrice;
+// };
+
+
+//////////////////////////////// №7 ////////////////////////
+
+// const filterArray = (numbers, value) => {
+//   const filteredNumbers = [];
+
+//   numbers.forEach((number) => {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+
+ 
+//   return filteredNumbers;
+// };
+
+//////////////////////////////// №8 ////////////////////////
+
+
+// const getCommonElements = (firstArray, secondArray) => {
+//     const commonElements = [];
+  
+//     firstArray.forEach((element) => {
+//       if (secondArray.includes(element)) {
+//         commonElements.push(element);
+//       }
+//     });
+  
+//     return commonElements;
+// }
+  
+//////////////////////////////// №9 ////////////////////////
